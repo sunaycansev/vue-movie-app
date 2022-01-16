2d1234ba600161c4656aa2e7344bd2f1e0086b4e
sunaycansev <fsunaycansev@gmail.com>
2022-01-16 13:44:43 +0300

    add movieService and plugin setup

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "@/router";
+import store from "@/store";
+import "./plugin/primeVue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+  router,
+  store,
+}).$mount("#app");
---
+++ src/services/movieService.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const movieService = axios.create({
+  baseURL: process.env.VUE_APP_BASE_API_URL,
+});
+
+export default {
+  getPopularMovies() {
+    return movieService.get(
+      `/movie/popular?api_key=${process.env.VUE_APP_API_KEY}`
+    );
+  },
+  getMovieDetail(id) {
+    return movieService.get(
+      `/movie/${id}?api_key=${process.env.VUE_APP_API_KEY}`
+    );
+  },
+};
